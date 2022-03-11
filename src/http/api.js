026005d067fd75d3d2aba98d162bc4f9c5c0f985
PraiseDelia <elemdelia@gmail>
2022-03-11 21:39:25 +0100

    updated API url

--- src/http/api.js
+++ src/http/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem("token");
 
 const instance = axios.create({
   // baseURL: 'http://localhost:8000/api/v1',
-  baseURL: "https://WeCare-api.herokuapp.com/api/v1",
+  baseURL: "https://arthemcare-api.herokuapp.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
